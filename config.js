7259a3081c5405858a9e8aeeee8666a667ce8a8c
Moustapha-xdroid2010 <moustaphax2010@gmail.com>
2025-06-06 11:38:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '263780166288';
+const defaultOwner = '221757637286';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '263780166288' ; // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '221757537286' ; // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'ᴊɪɴᴡᴏᴏ ᴠ4';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '221757537286' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'hiUEFAwI#rnDMMC-FpHMrEyHkETLcP6OFGiHVSETAZBfCRIkiHLI' ;  // put your session id here
 
 global.mods = []
 global.prems = []
